9a6356de18c06417e70fa4e6e076d5a755c6cfb6
David Rodenas <david.rodenas+gt@gmail.com>
2022-03-13 10:20:13 +0100

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 5001;
 
 const todos = [
   {
     id: "ad1a81f5-c327-47dd-9c6b-e6c63f47e4ff",
     title: "Afegir un nou todo",
-    details: "Al començar ja en tenim, pero en volem més.",
+    details:
+      "Al començar ja en tenim, pero en volem més.",
     completed: false,
   },
   {
@@ -24,6 +26,7 @@ const todos = [
 ];
 
 express()
+  .use(cors())
   .use(express.text())
   .get("/todos/", (req, res) => {
     const offset = +(req.query.offset || 0);
@@ -33,27 +36,41 @@ express()
     if (isTrue(req.query.completed))
       result = result.filter((todo) => todo.completed);
     if (isFalse(req.query.completed))
-      result = result.filter((todo) => !todo.completed);
+      result = result.filter(
+        (todo) => !todo.completed
+      );
     if (req.query.title)
       result = result.filter((todo) =>
         todo.title
           .toLowerCase()
-          .includes(req.query.title.toLocaleLowerCase().trim())
+          .includes(
+            req.query.title.toLocaleLowerCase().trim()
+          )
       );
     if (req.query.details)
       result = result.filter((todo) =>
         todo.details
           .toLowerCase()
-          .includes(req.query.details.toLocaleLowerCase().trim())
+          .includes(
+            req.query.details
+              .toLocaleLowerCase()
+              .trim()
+          )
       );
 
     res.json(result.slice(offset, offset + limit));
   })
   .get("/todos/:postId", (req, res) => {
-    res.json(todos.filter((todo) => todo.id === req.params.postId)[0]);
+    res.json(
+      todos.filter(
+        (todo) => todo.id === req.params.postId
+      )[0]
+    );
   })
   .post("/todos/", (req, res) => {
-    const { title, details, completed } = JSON.parse(req.body);
+    const { title, details, completed } = JSON.parse(
+      req.body
+    );
     const receivedTodo = { title, details, completed };
 
     const newTodo = {
@@ -67,31 +84,50 @@ express()
     res.json(newTodo);
   })
   .post("/todos/:postId", (req, res) => {
-    const index = todos.findIndex((todo) => todo.id === req.params.postId);
+    const index = todos.findIndex(
+      (todo) => todo.id === req.params.postId
+    );
     if (index < 0) throw new Error("Not Found");
 
-    const { title, details, completed } = JSON.parse(req.body);
+    const { title, details, completed } = JSON.parse(
+      req.body
+    );
     const receivedTodo = { title, details, completed };
 
-    todos[index] = { ...todos[index], ...receivedTodo };
+    todos[index] = {
+      ...todos[index],
+      ...receivedTodo,
+    };
     res.json(todos[index]);
   })
   .delete("/todos/:postId", (req, res) => {
-    const index = todos.findIndex((todo) => todo.id === req.params.postId);
+    const index = todos.findIndex(
+      (todo) => todo.id === req.params.postId
+    );
     if (index < 0) throw new Error("Not Found");
     const deletedTodo = todos.splice(index, 1);
     res.json(deletedTodo);
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(PORT, () =>
+    console.log(`Listening on ${PORT}`)
+  );
 
 function isTrue(value) {
   if (!value) return;
   value = value.toLowerCase();
-  return value === "true" || value === "1" || value === "yes";
+  return (
+    value === "true" ||
+    value === "1" ||
+    value === "yes"
+  );
 }
 
 function isFalse(value) {
   if (!value) return;
   value = value.toLowerCase();
-  return value === "false" || value === "0" || value === "no";
+  return (
+    value === "false" ||
+    value === "0" ||
+    value === "no"
+  );
 }
